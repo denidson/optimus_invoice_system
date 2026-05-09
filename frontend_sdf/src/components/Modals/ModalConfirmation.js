90773abd766835d94d2cf018cabdfe952c9fcb89
Monica Mujica <miml.soc2115@gmail.com>
2026-05-09 08:41:19 -0400

    Deploy

--- frontend_sdf/src/components/Modals/ModalConfirmation.js
+++ frontend_sdf/src/components/Modals/ModalConfirmation.js
@@ -23,7 +23,7 @@ const overlayStyle = {
   left: 0,
   right: 0,
   bottom: 0,
-  backgroundColor: "rgba(0, 0, 0, 0.5)", // Fondo oscuro
+  backgroundColor: "rgba(0, 0, 0, 0.5)", // Fondo oscuro 
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
